eff6527cae3249e5e496ee7efe5b5e142db4f5d4
ElenaFernandez <elena.fernandez92@gmail.com>
2021-12-14 09:04:49 +0100

    add index

---
+++ js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const playBTn = document.querySelector('.js_button');
+const selectOpstion = document.querySelector('.js_options');
+const textValue = document.querySelector('.text');
+const player = document.querySelector('.js_jugador');
+const computer = document.querySelector('.js_computadora');
+
+
+
+
+
+
+
+
+
+
+playBTn.addEventListener ('click', handleClickButton);
